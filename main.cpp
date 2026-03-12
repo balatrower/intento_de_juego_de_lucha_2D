bb0d3affb699a9f350a080d2509cc731aed6f585
balatrower <power333xd@gmail.com>
2026-03-13 00:08:13 +0100

    updated cmakelists.txt to compile sfml library and tried to make a loop in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
+#include <>
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Works!");
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    // 2. The Main Game Loop - runs as long as the window is open
+    while (window.isOpen()) {
+
+        // 3. Event Handling (The "Input" phase)
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // If the user clicks the 'X', close the window
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        // 4. Update Logic (The "Update" phase)
+        // This is where you'd move players or check collisions later.
+
+        // 5. Rendering (The "Draw" phase)
+        window.clear(sf::Color::Black); // Pick a background color
+
+        // Everything you draw goes between clear() and display()
+
+        window.display(); // Push everything to the screen
     }
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
